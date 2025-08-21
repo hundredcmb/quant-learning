125a3aa6f33b2a6af82df48db264a6b0429726ea
SHUCAN\LSY <2958719009@qq.com>
2025-08-21 19:31:08 +0800

    新增 MACD 指标计算

--- vnpy_examples/04_indicator.py
+++ vnpy_examples/04_indicator.py
@@ -18,9 +18,12 @@ def indicator_kdj_example(
     - 底层使用 TA-lib 库, 参数需要经过处理才能传入
     - 注意: 滑动窗口中的有效 bardata 至少需要 40 个
     """
+    # 创建一个滑动窗口并补充数据
     am = ArrayManager(size=40)
     for bar in bardata_list:
-        am.update_bar(bar=bar)  # 移动窗口并插入一个 bardata 数据
+        am.update_bar(bar=bar)
+
+    # 计算指标
     k, d = am.stoch(
         fastk_period=fastk_period,
         slowk_period=slowk_period * 2 - 1,
@@ -29,14 +32,43 @@ def indicator_kdj_example(
         slowd_matype=1,
     )
     j = 3 * k - 2 * d
+
     f_time = bardata_list[-1].datetime.strftime("%Y-%m-%d")
     code = f"{bardata_list[-1].symbol}.{bardata_list[-1].exchange.value}"
     logger.info(f"{f_time} {code} (K, D, J) = ({k:.2f}, {d:.2f}, {j:.2f})")
     return k, d, j
 
 
-def indicator_macd_example(bardata_list: list[BarData]) -> None:
-    pass
+def indicator_macd_example(
+    bardata_list: list[BarData],
+    fast_period: int = 12,
+    slow_period: int = 26,
+    signal_period: int = 9,
+) -> tuple[float, float, float]:
+    """
+    - 计算 bardata_list 中最后一天的 MACD 指标的 3 个值 (DIF, DEA, MACD/BAR)
+    - 结果与东方财富以及同花顺相同
+    - 底层使用 TA-lib 库
+    - 涉及股价的计算, 对比数据要使用不复权价格
+    - 注意: 滑动窗口中的有效 bardata 至少需要 120 个
+    """
+    # 创建一个滑动窗口并补充数据
+    am = ArrayManager(size=120)
+    for bar in bardata_list:
+        am.update_bar(bar=bar)
+
+    # 计算指标
+    dif, dea, hist = am.macd(
+        fast_period=fast_period,
+        slow_period=slow_period,
+        signal_period=signal_period,
+    )
+    bar = 2 * (dif - dea)
+
+    f_time = bardata_list[-1].datetime.strftime("%Y-%m-%d")
+    code = f"{bardata_list[-1].symbol}.{bardata_list[-1].exchange.value}"
+    logger.info(f"{f_time} {code} (DIF, DEA, MACD/BAR) = ({dif:.2f}, {dea:.2f}, {bar:.2f})")
+    return dif, dea, bar
 
 
 def indicator_bbi_example(
@@ -48,18 +80,23 @@ def indicator_bbi_example(
 ) -> float:
     """
     - 计算 bardata_list 中最后一天的 bbi 值
-    - 结果与东方财富相同
+    - 结果与东方财富以及同花顺相同
+    - 底层使用 TA-lib 库
     - 涉及股价的计算, 对比数据要使用不复权价格
     - 注意: 滑动窗口中的有效 bardata 至少需要 p4(默认24) 个
     """
+    # 创建一个滑动窗口并补充数据
     am = ArrayManager(size=24)
     for bar in bardata_list:
-        am.update_bar(bar=bar)  # 移动窗口并插入一个 bardata 数据
+        am.update_bar(bar=bar)
+
+    # 计算指标
     ma3 = am.sma(n=p1)
     ma6 = am.sma(n=p2)
     ma12 = am.sma(n=p3)
     ma24 = am.sma(n=p4)
     bbi = (ma3 + ma6 + ma12 + ma24) / 4
+
     f_time = bardata_list[-1].datetime.strftime("%Y-%m-%d")
     code = f"{bardata_list[-1].symbol}.{bardata_list[-1].exchange.value}"
     logger.info(f"{f_time} {code} BBI = {bbi:.2f}")
@@ -73,9 +110,10 @@ if __name__ == '__main__':
         symbol="600036",
         exchange=Exchange.SSE,
         interval=Interval.DAILY,
-        start=datetime(2024, 12, 1),
+        start=datetime(2024, 7, 1),
         end=datetime(2025, 2, 28),
     )
 
     indicator_kdj_example(bardata_list=b_list)
     indicator_bbi_example(bardata_list=b_list)
+    indicator_macd_example(bardata_list=b_list)
