023bee332196e5ad1bebaa7b96a6a4e70878dfbc
SHUCAN\LSY <2958719009@qq.com>
2025-08-21 18:48:17 +0800

    新增 BBI 指标计算

--- vnpy_examples/04_indicator.py
+++ vnpy_examples/04_indicator.py
@@ -16,7 +16,7 @@ def indicator_kdj_example(
     - 计算 bardata_list 中最后一天的 kdj 值
     - 结果与东方财富以及同花顺相同
     - 底层使用 TA-lib 库, 参数需要经过处理才能传入
-    - 注意: 滑动窗口中的有效 bardata 至少需要 37 个
+    - 注意: 滑动窗口中的有效 bardata 至少需要 40 个
     """
     am = ArrayManager(size=40)
     for bar in bardata_list:
@@ -39,8 +39,31 @@ def indicator_macd_example(bardata_list: list[BarData]) -> None:
     pass
 
 
-def indicator_bbi_example(bardata_list: list[BarData]) -> None:
-    pass
+def indicator_bbi_example(
+    bardata_list: list[BarData],
+    p1: int = 3,
+    p2: int = 6,
+    p3: int = 12,
+    p4: int = 24,
+) -> float:
+    """
+    - 计算 bardata_list 中最后一天的 bbi 值
+    - 结果与东方财富相同
+    - 涉及股价的计算, 对比数据要使用不复权价格
+    - 注意: 滑动窗口中的有效 bardata 至少需要 p4(默认24) 个
+    """
+    am = ArrayManager(size=24)
+    for bar in bardata_list:
+        am.update_bar(bar=bar)  # 移动窗口并插入一个 bardata 数据
+    ma3 = am.sma(n=p1)
+    ma6 = am.sma(n=p2)
+    ma12 = am.sma(n=p3)
+    ma24 = am.sma(n=p4)
+    bbi = (ma3 + ma6 + ma12 + ma24) / 4
+    f_time = bardata_list[-1].datetime.strftime("%Y-%m-%d")
+    code = f"{bardata_list[-1].symbol}.{bardata_list[-1].exchange.value}"
+    logger.info(f"{f_time} {code} BBI = {bbi:.2f}")
+    return bbi
 
 
 if __name__ == '__main__':
@@ -50,8 +73,9 @@ if __name__ == '__main__':
         symbol="600036",
         exchange=Exchange.SSE,
         interval=Interval.DAILY,
-        start=datetime(2025, 1, 1),
+        start=datetime(2024, 12, 1),
         end=datetime(2025, 2, 28),
     )
 
     indicator_kdj_example(bardata_list=b_list)
+    indicator_bbi_example(bardata_list=b_list)
